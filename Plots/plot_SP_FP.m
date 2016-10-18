a82a34005d8656d3359c16d0bd0e17c003a9e90a
Susan Nazirizadeh <sn4n13@soton.ac.uk>
2016-10-18 14:39:53 +0100

    3D plots, speed added

--- Plots/plot_SP_FP.m
+++ Plots/plot_SP_FP.m
@@ -1,20 +1,9 @@
-%% Per weight
+
  load mjerk_xyz_int
-weight= [73.815 87.34 69.69 85.1 70.4 80.5 72.37 93.49 78.2 85.8 78.1 73.1];
-
-BWper=[ 0.2 0.5 0.7 0.8 0.9 1.0];
-
-% BWweight= weight* BWper;
-
-for i=1:6
-    for j=1:12
-    BWperweight(i,j)=weight(j)*BWper(i);
-    end 
-end
 
 %% Display the mean of the jerk per weight
 speed = [5 8 12];
-device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
+% device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
 % figure;
 %  for n= 1:length(device)
 for o=1:length(speed)
@@ -36,73 +25,103 @@ for o=1:length(speed)
         catch
             display(['fitting error for participant ' , num2str(m)])
         end
-        
+        display(f)
         plot (mjerk_xyz_int2(1:6,1:6),mjerk_xyz_int1(1:6,1:6),'*',mjerk_xyz_int2(1:6,7:12),mjerk_xyz_int1(1:6,7:12),'o')
-        plot(f);
-        title(['Jerk of Smartphone on the hip and force plate, ' num2str(speed(o)) 'km/h'])
-        xlabel('Jerk force plate [N]')
+        plot(f,'-');
+        title(['Jerk of Smartphone between the sholder baldes and force plate, ' num2str(speed(o)) 'km/h'])
+        xlabel('Ground reaction force by force plate [N]')
         ylabel('Jerk device [m/s^3]')
-        legend ('Participant 1','Participant 1','Participate 2','Participant 2','Participate 3','Participant 3','Participate 4','Participant 4','Participate 5','Participant 5','Participate 6','Participant 6','Participate 7','Participant 7','Participate 8','Participant 8','Participate 9','Participant 9','Participate 10','Participant 10','Participate 11','Participant 11','Participate 12','Participant 12','Location','southeast')
+        legend ('Participate 2','Participate 3','Participate 4','Participate 5','Participate 6','Participate 7','Participate 8','Participate 9','Participate 10','Participate 11','Participate 12','Location','northwest')
         grid on
         hold on
         %    axis([10 100  0 300000])
-        filename= (['jerk_SP1_FP_' num2str(speed(o)) 'km.pdf']);
+        filename= (['jerk_SP2_FP_' num2str(speed(o)) 'km.pdf']);
         print(filename,'-dpdf')
     end
 end
 %  end
 
-%% Display the fitted line 
+%% Display the mean of the jerk per weight
 speed = [5 8 12];
 device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
-% figure;
-%  for n= 1:length(device)
-for o=1:length(speed)
-    figure;
-    for m= 2:12
-        
-        try
-            f=fit(mjerk_xyz_int2(:,m),mjerk_xyz_int1(:,m),'poly1');
-        catch
-            display(['fitting error for participant ' , num2str(m)])
-        end
-        
-        plot (mjerk_xyz_int2(1:6,1:6),mjerk_xyz_int1(1:6,1:6),'*',mjerk_xyz_int2(1:6,7:12),mjerk_xyz_int1(1:6,7:12),'o')
-        plot(f);
-        %          plot (f,mjerk_xyz_int2(1:6,1:6),mjerk_xyz_int1(1:6,1:6),'*',f,mjerk_xyz_int2(1:6,7:12),mjerk_xyz_int1(1:6,7:12),'o')
-        title(['Jerk of Smartphone on the hip and force plate, ' num2str(speed(o)) 'km/h'])
-        xlabel('Jerk force plate [N]')
-        ylabel('Jerk device [m/s^3]')
-        legend ('Participant 1','Participate 2','Participate 3','Participate 4','Participate 5','Participate 6','Participate 7','Participate 8','Participate 9','Participate 10','Participate 11','Participate 12','Location','southeast')
-        grid on
-        hold on
-        %    axis([10 100  0 300000])
-        filename= (['jerk_SP1_FP_' num2str(speed(o)) 'km.pdf']);
-        print(filename,'-dpdf')
-    end
-end
-%  end
-%%
-Xtotal=NaN(72,3);
-Ytotal=NaN(72,3);
-for o=1:3
-X= NaN(6,12);
-Y= NaN(6,12);
-
+participant= [1 2 3 4 5 6 7 8 9 10 11 12];
 for m=1:12
-    for p=1:6
-        if isempty( mjerk_xyz_int.treadmill{m}{3}{o}{p} ) ~= 1
-            if isempty( mjerk_xyz_int.treadmill{m}{5}{o}{p} ) ~= 1
-                X(p,m)=mjerk_xyz_int.treadmill{m}{5}{o}{p};
-                Y(p,m)=mjerk_xyz_int.treadmill{m}{3}{o}{p};
-
+    results{m}= NaN(18,3);
+end 
+i=1;
+for m= 1:12
+    figure;
+    for  p=1:6
+        if isempty( mjerk_xyz_int.treadmill{m}{3}{1}{p} ) ~= 1
+            if isempty( mjerk_xyz_int.treadmill{m}{3}{2}{p} ) ~= 1
+                if isempty( mjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1         
+                    results{m}(:,1)= cell2mat([mjerk_xyz_int.treadmill{m}{5}{1}(1:6)';mjerk_xyz_int.treadmill{m}{5}{2}(1:6)';mjerk_xyz_int.treadmill{m}{5}{3}(1:6)']);
+                    results{m}(:,2)= cell2mat([mjerk_xyz_int.treadmill{m}{3}{1}(1:6)';mjerk_xyz_int.treadmill{m}{3}{2}(1:6)';mjerk_xyz_int.treadmill{m}{3}{3}(1:6)']);
+                    results{m}(:,3)= ([5,5,5,5,5,5,8,8,8,8,8,8,12,12,12,12,12,12]);
+                end
             end
         end
     end
+    try
+        mld=fitlm(results{m}(:,2:3),results{m}(:,1));
+    catch
+        display(['fitting error for participant ' , num2str(m)])
+    end
+    i=i+1;
+    display(i)
+    display(mld)
+            scatter3 (results{m}(1:6,3),results{m}(1:6,1),results{m}(1:6,2),'g')
+%             ,results{m}(7:12,3),results{m}(7:12,1),results{m}(7:12,2),'g',results{m}(13:18,3),results{m}(13:18,1),results{m}(13:18,2),'b')
+        title(['Jerk of Smartphone between the sholder baldes and force plate,  P' num2str(participant(m))])
+        xlabel('Speed [m/s]')
+        ylabel('Ground reaction force by force plate [N]')
+        zlabel('Jerk from smartphone between sholder baldes [m/s^3]')
+        legend ('5km/h','8km/h','12km/h','Location','northwest')
+        grid on
+        hold on
+        %    axis([10 100  0 300000])
+        filename= (['jerk_SP2_FP_' num2str(speed(o)) 'km.pdf']);
+        print(filename,'-dpdf')
+        hold on
+        grid on
 end
-                Xtotal(:,o)=[X(:,1);X(:,2);X(:,3);X(:,4);X(:,5);X(:,6);X(:,7);X(:,8);X(:,9);X(:,10);X(:,11);X(:,12)];
-                Ytotal(:,o)=[Y(:,1);Y(:,2);Y(:,3);Y(:,4);Y(:,5);Y(:,6);Y(:,7);Y(:,8);Y(:,9);Y(:,10);Y(:,11);Y(:,12)];
-end
 
-
-
+% %% Display FP, jerk SP and speed
+% speed = [5 8 12];
+% % device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
+% % figure;
+% %  for n= 1:length(device)
+% for o=1:length(speed)
+%     figure;
+%     mjerk_xyz_int1= NaN(6,12);
+%     mjerk_xyz_int2= NaN(6,12);
+%     for m= 2:12
+%         for p= 1:6
+%             if isempty( mjerk_xyz_int.treadmill{m}{3}{o}{p} ) ~= 1
+%                 if isempty( mjerk_xyz_int.treadmill{m}{5}{o}{p} ) ~= 1
+%                     mjerk_xyz_int1(p,m)= mjerk_xyz_int.treadmill{m}{3}{o}{p};
+%                     mjerk_xyz_int2(p,m)= mjerk_xyz_int.treadmill{m}{5}{o}{p};
+%                 end
+%             end
+%         end   
+%         
+%         try
+%             f=fit(mjerk_xyz_int2(:,m),mjerk_xyz_int1(:,m),'poly1');
+%         catch
+%             display(['fitting error for participant ' , num2str(m)])
+%         end
+%         display(f)
+%         plot (mjerk_xyz_int2(1:6,1:6),mjerk_xyz_int1(1:6,1:6),'*',mjerk_xyz_int2(1:6,7:12),mjerk_xyz_int1(1:6,7:12),'o')
+%         plot(f,'-');
+%         title(['Jerk of Smartphone between the sholder baldes and force plate, ' num2str(speed(o)) 'km/h'])
+%         xlabel('Ground reaction force by force plate [N]')
+%         ylabel('Jerk device [m/s^3]')
+%         legend ('Participate 2','Participate 3','Participate 4','Participate 5','Participate 6','Participate 7','Participate 8','Participate 9','Participate 10','Participate 11','Participate 12','Location','northwest')
+%         grid on
+%         hold on
+%         %    axis([10 100  0 300000])
+%         filename= (['jerk_SP2_FP_' num2str(speed(o)) 'km.pdf']);
+%         print(filename,'-dpdf')
+%     end
+% end
+% %  end
