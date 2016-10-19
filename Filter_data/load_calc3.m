5a84205291d9110bf1fc307f85908153c5381cd3
Susan Nazirizadeh <sn4n13@soton.ac.uk>
2016-10-19 14:19:18 +0100

    Cleaned the code for better read, Send this version to Alex to correct the problem with the load rate from the force plate data

--- Filter_data/load_calc3.m
+++ Filter_data/load_calc3.m
@@ -1,21 +1,21 @@
 load raw_int
 %% Transfer into j_xyz part 2
-% Allocat jerk_xyz_int_data.treadmill
-jerk_xyz_int.treadmill= cell(1,12);
+% Allocat jerkjerk_xyz_int_data.treadmill
+jerkjerk_xyz_int.treadmill= cell(1,12);
 for m= 1:12
-    jerk_xyz_int.treadmill{m}= cell(1,5);
+    jerkjerk_xyz_int.treadmill{m}= cell(1,5);
     for n=1:5
-    jerk_xyz_int.treadmill{m}{n}= cell(1,3);
+    jerkjerk_xyz_int.treadmill{m}{n}= cell(1,3);
         for o=1:3
-        jerk_xyz_int.treadmill{m}{n}{o}= cell(1,6);
+        jerkjerk_xyz_int.treadmill{m}{n}{o}= cell(1,6);
             for p=1:6
-                jerk_xyz_int.treadmill{m}{n}{o}{p}= [];
+                jerkjerk_xyz_int.treadmill{m}{n}{o}{p}= [];
             end
         end
     end
 end
 
-%% Transform raw_int into jerk in gravity direction
+%% Transform raw_int into jerk in original direction
 % load aload
 for m= 1:12
     for n=1:4%5
@@ -25,8 +25,8 @@ for m= 1:12
                     for p=1:6
                         disp('working')
                         if isempty( raw_int.treadmill{m}{n}{o}{p}) ~= 1
-                                jerk_xyz_int.treadmill{m}{n}{o}{p}(:,2)=jerk_xyz(raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,1),raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,:));
-                                jerk_xyz_int.treadmill{m}{n}{o}{p}(:,1)=raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,1);
+                                jerkjerk_xyz_int.treadmill{m}{n}{o}{p}(:,2)=jerk_xyz(raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,1),raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,:));
+                                jerkjerk_xyz_int.treadmill{m}{n}{o}{p}(:,1)=raw_int.treadmill{m}{n}{o}{p}(1000:end-1000,1);
                         end
                     end
                 end
@@ -35,30 +35,30 @@ for m= 1:12
     end
 end
 
-%% Entering the force plate data 
+%% Entering the force plate data using "jerk" function or below just using raw force plate data
 for m= 1:12
     if isempty(raw_int.treadmill{m}{5}) ~= 1
         for o=1:3
             if isempty( raw_int.treadmill{m}{5}{o}) ~= 1
                 for p=1:6
                     if isempty( raw_int.treadmill{m}{5}{o}{p}) ~= 1
-                        jerk_xyz_int.treadmill{m}{5}{o}{p}(:,2)=jerk(raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1),raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,6));
-                        jerk_xyz_int.treadmill{m}{5}{o}{p}(:,1)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1);
+                        jerkjerk_xyz_int.treadmill{m}{5}{o}{p}(:,2)=jerk(raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1),raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,6));
+                        jerkjerk_xyz_int.treadmill{m}{5}{o}{p}(:,1)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1);
                     end
                 end
             end
         end
     end
 end
-% %% Entering the force plate data 
+% %% Entering the force plate data  
 % for m= 1:12
 %     if isempty(raw_int.treadmill{m}{5}) ~= 1
 %         for o=1:3
 %             if isempty( raw_int.treadmill{m}{5}{o}) ~= 1
 %                 for p=1:6
 %                     if isempty( raw_int.treadmill{m}{5}{o}{p}) ~= 1
-%                         jerk_xyz_int.treadmill{m}{5}{o}{p}(:,2)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,6);
-%                         jerk_xyz_int.treadmill{m}{5}{o}{p}(:,1)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1);
+%                         jerkjerk_xyz_int.treadmill{m}{5}{o}{p}(:,2)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,6);
+%                         jerkjerk_xyz_int.treadmill{m}{5}{o}{p}(:,1)=raw_int.treadmill{m}{5}{o}{p}(1000:end-1000,1);
 %                     end
 %                 end
 %             end
@@ -67,34 +67,34 @@ end
 % end
 %% Develope mean of the data 
 % Allocate mjerk_xyz
-mjerk_xyz_int.treadmill= cell(1,12);                                                    
+mjerkjerk_xyz_int.treadmill= cell(1,12);                                                    
 for m= 1:12
-    mjerk_xyz_int.treadmill{m}= cell(1,5);
+    mjerkjerk_xyz_int.treadmill{m}= cell(1,5);
     for n=1:5
-    mjerk_xyz_int.treadmill{m}{n}= cell(1,3);
+    mjerkjerk_xyz_int.treadmill{m}{n}= cell(1,3);
         for o=1:3
-        mjerk_xyz_int.treadmill{m}{n}{o}= cell(1,6);
+        mjerkjerk_xyz_int.treadmill{m}{n}{o}= cell(1,6);
             for p=1:6
-                 mjerk_xyz_int.treadmill{m}{n}{o}{p}= [];
+                 mjerkjerk_xyz_int.treadmill{m}{n}{o}{p}= [];
              end
         end
     end
 end
 %%
-% Calculate mean of jerk_xyz_int in the gravity direction (in this case 4th
+% Calculate mean of jerkjerk_xyz_int in the gravity direction (in this case 4th
 % colum)
 for m= 1:12
     for n=1:5
-        if isempty(jerk_xyz_int.treadmill{m}{n}) ~= 1
+        if isempty(jerkjerk_xyz_int.treadmill{m}{n}) ~= 1
             for o=1:3
-                if isempty(jerk_xyz_int.treadmill{m}{n}{o}) ~= 1
+                if isempty(jerkjerk_xyz_int.treadmill{m}{n}{o}) ~= 1
                     for p=1:6
-                        if isempty(jerk_xyz_int.treadmill{m}{n}{o}{p}) ~= 1
-                            data = jerk_xyz_int.treadmill{m}{n}{o}{p}(:,2);
+                        if isempty(jerkjerk_xyz_int.treadmill{m}{n}{o}{p}) ~= 1
+                            data = jerkjerk_xyz_int.treadmill{m}{n}{o}{p}(:,2);
                             
                             if length(data) > 0
                                 display(mean(data));
-                                mjerk_xyz_int.treadmill{m}{n}{o}{p}= mean(data);
+                                mjerkjerk_xyz_int.treadmill{m}{n}{o}{p}= mean(data);
                             end
                         end
                     end
@@ -127,9 +127,9 @@ device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
         result= NaN(6,12);
         for m=1:11
             for p= 1:6
-                if isempty( mjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1
+                if isempty( mjerkjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1
                     
-                    result(p,m)= mjerk_xyz_int.treadmill{m}{3}{3}{p};
+                    result(p,m)= mjerkjerk_xyz_int.treadmill{m}{3}{3}{p};
                 end
             end
         end
@@ -144,31 +144,12 @@ device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
             
             hold on
             scatter(x,y)
-%             plot(x,yCalc1)
             title('Smartphone jerk value in vertical directions 12km/h')
             xlabel('Body weight [kg]')
             ylabel('Jerk [m/s^3]')
             legend ('Participant 1','Participate 2','Participate 3','Participate 4','Participate 5','Participate 6','Participate 7','Participate 8','Participate 9','Participate 10','Participate 11','Participate 12','Location','southeast')
-%             legend ('Participant 1','Line 1','Participate 2','Line 2','Participate 3','Line 3','Participate 4','Line 4','Participate 5','Line 5','Participate 6','Line 6','Participate 7','Line 7','Participate 8','Line 8','Participate 9','Line 9','Participate 10','Line 10','Participate 11','Line 11','Location','southeast')
             grid on
-%             Rsq(m,1)= 1 - sum((y - yCalc1).^2)/sum((y - mean(y)).^2);
+
             
         end
-        
-%         
-%        for m=1:11 
-%         R=corrcoef(BWperweight(1:6,m),result(1:6,m));
-%         display= R;
-%        end 
-
-%         
-%         figure;
-%         plot(BWperweight(1:6,1:12),result(1:6,1:12),'o')
-%         title(['Calculated Load vs. bodyweight in gravity direction, ' num2str(speed(o)) 'km/h, ' device{n}])
-%         
-%         %    axis([10 100  0 300000])
-%         filename= (['jerk_xyz_int_' num2str(speed(o)) 'km_' device{n} '.pdf']);
-% %         print(filename,'-dpdf')
-%     end
-% end
 
--- Plots/plot_SP_FP.m
+++ Plots/plot_SP_FP.m
@@ -1,62 +1,63 @@
-
- load mjerk_xyz_int
-
-%% Display the mean of the jerk per weight
+load mjerk_xyz_int          % Data set with the mean of the data with jerk data from the phone and the force  of the force plate
+ load mjerkjerk_xyz_int     % Data set with the mean of the data with jerk data from the phone and the force rate of the force plate 
+ 
+%% Fit a line for smartphone and forceplate data 
 speed = [5 8 12];
-% device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
-% figure;
-%  for n= 1:length(device)
+
 for o=1:length(speed)
     figure;
-    mjerk_xyz_int1= NaN(6,12);
-    mjerk_xyz_int2= NaN(6,12);
+    mjerkjerk_xyz_int1= NaN(6,12);
+    mjerkjerk_xyz_int2= NaN(6,12);
     for m= 2:12
         for p= 1:6
-            if isempty( mjerk_xyz_int.treadmill{m}{3}{o}{p} ) ~= 1
-                if isempty( mjerk_xyz_int.treadmill{m}{5}{o}{p} ) ~= 1
-                    mjerk_xyz_int1(p,m)= mjerk_xyz_int.treadmill{m}{3}{o}{p};
-                    mjerk_xyz_int2(p,m)= mjerk_xyz_int.treadmill{m}{5}{o}{p};
+            if isempty( mjerkjerk_xyz_int.treadmill{m}{3}{o}{p} ) ~= 1
+                if isempty( mjerkjerk_xyz_int.treadmill{m}{5}{o}{p} ) ~= 1
+                    mjerkjerk_xyz_int1(p,m)= mjerkjerk_xyz_int.treadmill{m}{3}{o}{p};   % Smartphone data combined into a matrix for the linear regression
+                    mjerkjerk_xyz_int2(p,m)= mjerkjerk_xyz_int.treadmill{m}{5}{o}{p};   % Force plate data combined into a matrix for the linear regression
                 end
             end
         end   
         
         try
-            f=fit(mjerk_xyz_int2(:,m),mjerk_xyz_int1(:,m),'poly1');
+            f=fit(mjerkjerk_xyz_int2(:,m),mjerkjerk_xyz_int1(:,m),'poly1');             % Fitting model 
         catch
             display(['fitting error for participant ' , num2str(m)])
         end
         display(f)
-        plot (mjerk_xyz_int2(1:6,1:6),mjerk_xyz_int1(1:6,1:6),'*',mjerk_xyz_int2(1:6,7:12),mjerk_xyz_int1(1:6,7:12),'o')
-        plot(f,'-');
+        plot (mjerkjerk_xyz_int2(1:6,1:6),mjerkjerk_xyz_int1(1:6,1:6),'*',mjerkjerk_xyz_int2(1:6,7:12),mjerkjerk_xyz_int1(1:6,7:12),'o') % Plot data points
+        hold on
+        plot(f,'-');  % Plot linear regression line                                                                                                                      
+        hold on
         title(['Jerk of Smartphone between the sholder baldes and force plate, ' num2str(speed(o)) 'km/h'])
         xlabel('Ground reaction force by force plate [N]')
         ylabel('Jerk device [m/s^3]')
         legend ('Participate 2','Participate 3','Participate 4','Participate 5','Participate 6','Participate 7','Participate 8','Participate 9','Participate 10','Participate 11','Participate 12','Location','northwest')
         grid on
-        hold on
-        %    axis([10 100  0 300000])
         filename= (['jerk_SP2_FP_' num2str(speed(o)) 'km.pdf']);
         print(filename,'-dpdf')
     end
 end
-%  end
 
-%% Display the mean of the jerk per weight
+%% Linear regression model with two variables
+    % Jerk from smartphone= responds variable
+    %  Load rate from force place= predictor variable 
+    % Speed= predictore variable
+    
 speed = [5 8 12];
 device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
 participant= [1 2 3 4 5 6 7 8 9 10 11 12];
 for m=1:12
     results{m}= NaN(18,3);
 end 
-i=0;
+i=0; % Participant counter
 for m= 1:11
     figure;
     for  p=1:6
-        if isempty( mjerk_xyz_int.treadmill{m}{3}{1}{p} ) ~= 1
-            if isempty( mjerk_xyz_int.treadmill{m}{3}{2}{p} ) ~= 1
-                if isempty( mjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1         
-                    results{m}(:,1)= cell2mat([mjerk_xyz_int.treadmill{m}{5}{1}(1:6)';mjerk_xyz_int.treadmill{m}{5}{2}(1:6)';mjerk_xyz_int.treadmill{m}{5}{3}(1:6)']);
-                    results{m}(:,2)= cell2mat([mjerk_xyz_int.treadmill{m}{3}{1}(1:6)';mjerk_xyz_int.treadmill{m}{3}{2}(1:6)';mjerk_xyz_int.treadmill{m}{3}{3}(1:6)']);
+        if isempty( mjerkjerk_xyz_int.treadmill{m}{3}{1}{p} ) ~= 1
+            if isempty( mjerkjerk_xyz_int.treadmill{m}{3}{2}{p} ) ~= 1
+                if isempty( mjerkjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1         
+                    results{m}(:,1)= cell2mat([mjerkjerk_xyz_int.treadmill{m}{5}{1}(1:6)';mjerkjerk_xyz_int.treadmill{m}{5}{2}(1:6)';mjerkjerk_xyz_int.treadmill{m}{5}{3}(1:6)']);
+                    results{m}(:,2)= cell2mat([mjerkjerk_xyz_int.treadmill{m}{3}{1}(1:6)';mjerkjerk_xyz_int.treadmill{m}{3}{2}(1:6)';mjerkjerk_xyz_int.treadmill{m}{3}{3}(1:6)']);
                     results{m}(:,3)= ([5,5,5,5,5,5,8,8,8,8,8,8,12,12,12,12,12,12]);
                 end
             end
@@ -64,91 +65,31 @@ for m= 1:11
     end
     
     A= results{m}(:,1:3);
-    R = corrcoef(A);
+    R = corrcoef(A);        % Correlation coefficiant 
     display(R);
     try
-        mld=fitlm(results{m}(:,2:3),results{m}(:,1));
+        mld=fitlm(results{m}(:,2:3),results{m}(:,1));   % Fit linear regresion model for two variables 
     catch
         display(['fitting error for participant ' , num2str(m)])
     end
-    i=i+1;
+    i=i+1; % Participant counter
     A= results{m}(:,1:3);
     R = corrcoef(A);
     display(i)
     display(mld)
-            scatter3 (results{m}(1:6,3),results{m}(1:6,1),results{m}(1:6,2),'g')
+            scatter3 (results{m}(1:6,3),results{m}(1:6,1),results{m}(1:6,2),'g')        % 3D scatter plots for 5km/h, forceplate data and smartphone jerk for each participant 
             hold on
-            scatter3(results{m}(7:12,3),results{m}(7:12,1),results{m}(7:12,2),'r')
+            scatter3(results{m}(7:12,3),results{m}(7:12,1),results{m}(7:12,2),'r')      % 3D scatter plots for 8km/h, forceplate data and smartphone jerk for each participant
             hold on
-            scatter3(results{m}(13:18,3),results{m}(13:18,1),results{m}(13:18,2),'b')
+            scatter3(results{m}(13:18,3),results{m}(13:18,1),results{m}(13:18,2),'b')   % 3D scatter plots for 12km/h, forceplate data and smartphone jerk for each participant
         title(['Jerk of Smartphone between the sholder baldes and force plate,  P' num2str(participant(m))])
         xlabel('Speed [m/s]')
-        ylabel('Ground reaction force by force plate [N]')
+        ylabel('Jerk from Ground reaction force by force plate [N/s]')
         zlabel('Jerk from smartphone between sholder baldes [m/s^3]')
         legend ('5km/h','8km/h','12km/h','Location','northwest')
         grid on
         hold on
-        %    axis([10 100  0 300000])
         filename= (['jerk_SP2_FP_speed' num2str(participant(m)) '.pdf']);
         print(filename,'-dpdf')
         grid on
-end
-
-%% Mixed linear model
-speed = [5 8 12];
-device  = cellstr(['SP1';'SW1';'SP2';'SW2';'FP ']);
-participant= [1 2 3 4 5 6 7 8 9 10 11 12];
-for m=1:12
-    results{m}= NaN(18,3);
-end
-i=0;
-for m= 1:11
-    figure;
-    for  p=1:6
-        if isempty( mjerk_xyz_int.treadmill{m}{3}{1}{p} ) ~= 1
-            if isempty( mjerk_xyz_int.treadmill{m}{3}{2}{p} ) ~= 1
-                if isempty( mjerk_xyz_int.treadmill{m}{3}{3}{p} ) ~= 1
-                    results{m}(:,1)= cell2mat([mjerk_xyz_int.treadmill{m}{5}{1}(1:6)';mjerk_xyz_int.treadmill{m}{5}{2}(1:6)';mjerk_xyz_int.treadmill{m}{5}{3}(1:6)']);
-                    results{m}(:,2)= cell2mat([mjerk_xyz_int.treadmill{m}{3}{1}(1:6)';mjerk_xyz_int.treadmill{m}{3}{2}(1:6)';mjerk_xyz_int.treadmill{m}{3}{3}(1:6)']);
-                    results{m}(:,3)= ([5,5,5,5,5,5,8,8,8,8,8,8,12,12,12,12,12,12]);
-                end
-            end
-        end
-    end
-    
-    
-    try
-        lme=fitlme(
-        mld=fitlm(results{m}(:,2:3),results{m}(:,1));
-    catch
-        display(['fitting error for participant ' , num2str(m)])
-    end
-    try
-        f=fit(results{m}(:,1),results{m}(:,2),'poly1');
-    catch
-        display(['fitting error for participant ' , num2str(m)])
-    end
-    i=i+1;
-    display(f)
-    A= results{m}(:,1:3);
-    R = corrcoef(A);
-    display(R);
-    display(i)
-    display(mld)
-    scatter3 (results{m}(1:6,3),results{m}(1:6,1),results{m}(1:6,2),'g')
-    hold on
-    scatter3(results{m}(7:12,3),results{m}(7:12,1),results{m}(7:12,2),'r')
-    hold on
-    scatter3(results{m}(13:18,3),results{m}(13:18,1),results{m}(13:18,2),'b')
-    title(['Jerk of Smartphone between the sholder baldes and force plate,  P' num2str(participant(m))])
-    xlabel('Speed [m/s]')
-    ylabel('Ground reaction force by force plate [N]')
-    zlabel('Jerk from smartphone between sholder baldes [m/s^3]')
-    legend ('5km/h','8km/h','12km/h','Location','northwest')
-    grid on
-    hold on
-    %    axis([10 100  0 300000])
-    filename= (['jerk_SP2_FP_speed' num2str(participant(m)) '.pdf']);
-    print(filename,'-dpdf')
-    grid on
 end
