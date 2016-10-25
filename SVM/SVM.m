13b86213b987f935bdcdabb64ca3be935a66e766
Susan Nazirizadeh <sn4n13@soton.ac.uk>
2016-10-25 16:03:48 +0100

    Started to write a SVM

---
+++ SVM/SVM.m
@@ -0,0 +1,18 @@
+%% Creat a SVM classfier
+% Optimize a Cross-Validated SVM Classifier Using Bayesian Optimization
+% 1. Prepare Data For Classification
+% 2. Prepare Cross-Validation
+% 3. Prepare Variables for Bayesian Optimization
+% 4. Objective Function
+% 5. Optimize Classifier
+% 6. Use the results to train a new, optimized SVM classifier
+% 7. Plot the classification boundaries
+% 8. Evaluate Accuracy on New Data
+load results
+C=([results{1}(:,:);results{2}(:,:);results{3}(:,:);results{4}(:,:);results{5}(:,:);results{6}(:,:);results{7}(:,:);results{8}(:,:);results{9}(:,:);results{10}(:,:);results{11}(:,:)]);
+%% Train SVM Classifier Using Custom Kernelfigure
+figure;
+gscatter(C(:,1),C(:,2),C(:,3));
+
+Mdl1 = fitcsvm(X,Y,'KernelFunction','mysigmoid','Standardize',true);
+
