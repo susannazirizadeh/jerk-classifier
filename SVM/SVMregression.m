56118be217f0f51886a19fd4ab2e0429468dcfe5
Nazirizadeh S <s.nazirizadeh@soton.ac.uk>
2016-10-31 14:55:05 +0000

    Continued trying  SVR

--- SVM/SVMregression.m
+++ SVM/SVMregression.m
@@ -2,24 +2,26 @@
 load results
 C=([results{1}(:,:);results{2}(:,:);results{3}(:,:);results{4}(:,:);results{5}(:,:);results{6}(:,:);results{7}(:,:);results{8}(:,:);results{9}(:,:);results{10}(:,:);results{11}(:,:)]);
 %% Example for SVM regression
-X=[C(25:end,1)];%,C(:,2)];
-Y=C(25:end,2);
+X=[C(:,2),C(:,4)];
+Y=C(:,1);
 MdlGau =fitrsvm(X,Y,'Standardize',true,'KernelFunction','gaussian');
-compactMdlLin=compact(MdlLin);
-yfit=predict(MdlGau,X)
-
+compactMdlLin=compact(MdlGau);
+yfit=predict(MdlGau,X);
 
 %% 
+Mdl= fitrsvm(X,Y); 
+compactMdl=compact(Mdl);
 MdlLin = fitrsvm(X,Y,'Standardize',true,'KFold',5);
 MdlGau = fitrsvm(X,Y,'KernelFunction','gaussian','KernelScale','auto','Standardize',true,'KFold',5);
 mseLin = kfoldLoss(MdlLin);
 mseGau = kfoldLoss(MdlGau);
-
-
-
-compactMdlLin=compact(MdlLin);
+L = resubLoss(Mdl);
+lossMSE= loss(Mdl,X,Y);
+lossEI = loss(Mdl,X,Y,'LossFun','epsiloninsensitive');
 
 %%
 figure;
-scatter(X,Y)
-
+scatter3(X(:,1),X(:,2),Y)
+ xlabel('Jerk data of smartphone between sholder baldes [m/s^3]')
+ ylabel('Bodyweight [kg]')
+ zlabel('Forceplate data [N]')
