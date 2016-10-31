657fc792ea5c22bce82d3ada0f16673a456bc3a4
Nazirizadeh S <s.nazirizadeh@soton.ac.uk>
2016-10-31 18:06:00 +0000

    Extended mixed effect model. Looked which parameters are significant. And compared each other

--- MixedModel/mixedmodel.m
+++ MixedModel/mixedmodel.m
@@ -53,31 +53,34 @@ text(.75,1.25,'Smartphone jerk and ground reaction force')
 plot(results{1}(1:6,1),results{1}(1:6,2),'*',results{1}(7:12,1),results{1}(7:12,2),'o',results{1}(13:18,1),results{1}(13:18,2),'p')
 hold on 
 plot(f1_1,'-');
-hold on
 plot(f1_2,'-');
-hold on
 plot(f1_3,'-');
-title('Participants 1')
+title('Participants 1');
+xlabel('Forceplate Data [N]');
+ylabel('Jerk Data from smartphone [m/s^3]');
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,2)
 plot(results{2}(1:6,1),results{2}(1:6,2),'*',results{2}(7:12,1),results{2}(7:12,2),'o',results{2}(13:18,1),results{2}(13:18,2),'p')
 hold on 
 plot(f2_1,'-');
-hold on
 plot(f2_2,'-');
-hold on
 plot(f2_3,'-');
 title('Participants 2')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,3)
 plot(results{3}(1:6,1),results{3}(1:6,2),'*',results{3}(7:12,1),results{3}(7:12,2),'o',results{3}(13:18,1),results{3}(13:18,2),'p')
 hold on 
 plot(f3_1,'-');
-hold on
 plot(f3_2,'-');
-hold on
 plot(f3_3,'-');
 title('Participants 3')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,4)
 plot(results{4}(1:6,1),results{4}(1:6,2),'*',results{4}(7:12,1),results{4}(7:12,2),'o',results{4}(13:18,1),results{4}(13:18,2),'p')
@@ -88,46 +91,54 @@ plot(f4_2,'-');
 hold on
 plot(f4_3,'-');
 title('Participants 4')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,5)
 plot(results{5}(1:6,1),results{5}(1:6,2),'*',results{5}(7:12,1),results{5}(7:12,2),'o',results{5}(13:18,1),results{5}(13:18,2),'p')
 hold on 
 plot(f5_1,'-');
-hold on
 plot(f5_2,'-');
-hold on
 plot(f5_3,'-');
 title('Participants 5')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,6)
 plot(results{6}(1:6,1),results{6}(1:6,2),'*',results{6}(7:12,1),results{6}(7:12,2),'o',results{6}(13:18,1),results{6}(13:18,2),'p')
 hold on 
 plot(f6_1,'-');
-hold on
 plot(f6_2,'-');
-hold on
 plot(f6_3,'-');
 title('Participants 6')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
+
 
 subplot(3,4,7)
 plot(results{7}(1:6,1),results{7}(1:6,2),'*',results{7}(7:12,1),results{7}(7:12,2),'o',results{7}(13:18,1),results{7}(13:18,2),'p')
 hold on 
 plot(f7_1,'-');
-hold on
 plot(f7_2,'-');
-hold on
 plot(f7_3,'-');
 title('Participants 7')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,8)
 plot(results{8}(1:6,1),results{8}(1:6,2),'*',results{8}(7:12,1),results{8}(7:12,2),'o',results{8}(13:18,1),results{8}(13:18,2),'p')
 hold on 
 plot(f8_1,'-');
-hold on
 plot(f8_2,'-');
-hold on
 plot(f8_3,'-');
 title('Participants 8')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,9)
 plot(results{9}(1:6,1),results{9}(1:6,2),'*',results{9}(7:12,1),results{9}(7:12,2),'o',results{9}(13:18,1),results{9}(13:18,2),'p')
@@ -135,29 +146,33 @@ hold on
 % plot(f9_1,'-');
 % hold on
 plot(f9_2,'-');
-hold on
 plot(f9_3,'-');
 title('Participants 9')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,10)
 plot(results{10}(1:6,1),results{10}(1:6,2),'*',results{10}(7:12,1),results{10}(7:12,2),'o',results{10}(13:18,1),results{10}(13:18,2),'p')
 hold on 
 plot(f10_1,'-');
-hold on
 plot(f10_2,'-');
-hold on
 plot(f10_3,'-');
 title('Participants 10')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,11)
 plot(results{11}(1:6,1),results{11}(1:6,2),'*',results{11}(7:12,1),results{11}(7:12,2),'o',results{11}(13:18,1),results{11}(13:18,2),'p')
 hold on 
 plot(f11_1,'-');
-hold on
 plot(f11_2,'-');
-hold on
 plot(f11_3,'-');
 title('Participants 11')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 subplot(3,4,12)
 plot(results{12}(1:6,1),results{12}(1:6,2),'*',results{12}(7:12,1),results{12}(7:12,2),'o',results{12}(13:18,1),results{12}(13:18,2),'p')
@@ -168,16 +183,25 @@ plot(results{12}(1:6,1),results{12}(1:6,2),'*',results{12}(7:12,1),results{12}(7
 % hold on
 % plot(f12_3,'-');
 title('Participants 12')
+xlabel('Forceplate Data [N]')
+ylabel('Jerk Data from smartphone [m/s^3]')
+legend ('Data points 5km/h','Data points 8km/h','Data points 12km/h','Location','northwest');
 
 filename= ('jerk_SP1_FP_each.pdf');
 print(filename,'-dpdf')
+
+%% Plot Forceplate data versus the jerk estimate
+C=([results{1}(:,:);results{2}(:,:);results{3}(:,:);results{4}(:,:);results{5}(:,:);results{6}(:,:);results{7}(:,:);results{8}(:,:);results{9}(:,:);results{10}(:,:);results{11}(:,:)]);
+figure;
+plot(C(:,2),C(:,1),'ro')
+xlabel('Forceplate Data [N]')
+ylabel('Estimated Jerk from smartphone [m/s^3]')
  
  %% 2.Linear regression model with random effect and without random effect
-C=([results{1}(:,:);results{2}(:,:);results{3}(:,:);results{4}(:,:);results{5}(:,:);results{6}(:,:);results{7}(:,:);results{8}(:,:);results{9}(:,:);results{10}(:,:);results{11}(:,:)]);
-tbl= table(C(:,1),C(:,2),C(:,3),C(:,4),C(:,5),'VariableNames',{'ForceRateFP','JerkSmartphone','Speed','BW','Participant'});
-lme1=  fitlme(tbl,'JerkSmartphone~ForceRateFP+Speed+BW+(1|Participant)+(ForceRateFP-1|Participant)');  % Mixed model with random effect intercept and coefficient 
-lme2=  fitlme(tbl,'JerkSmartphone~ForceRateFP+Speed+BW+(1|Participant)');     figure   % Mixed model with random effect intercept 
-lme3=  fitlme(tbl,'JerkSmartphone~ForceRateFP+Speed+BW');                       % Mixed model without random effect intercept
+tbl= table(C(25:end,1),C(25:end,2),C(25:end,3),C(25:end,4),C(25:end,5),'VariableNames',{'ForceRateFP','JerkSmartphone','Speed','BW','Participant'});
+lme1=  fitlme(tbl,'ForceRateFP~JerkSmartphone+Speed+BW+(1|Participant)+(JerkSmartphone-1|Participant)');  % Mixed model with random effect intercept and coefficient 
+lme2=  fitlme(tbl,'ForceRateFP~JerkSmartphone+Speed+BW+(1|Participant)');   % Mixed model with random effect intercept 
+lme3=  fitlme(tbl,'ForceRateFP~JerkSmartphone+Speed+BW');                       % Mixed model without random effect intercept
 
 %% 3.Check significants of random effect
 [psi,dispersion,stats] = covarianceParameters(lme1);
@@ -198,3 +222,19 @@ figure
 plotResiduals(lme1,'fitted','ResidualType','Pearson')
 
 %% 
+F = fitted(lme1);
+R = response(lme1);
+figure();
+plot(R,F,'rx')
+xlabel('Response')
+ylabel('Fitted')
+
+figure();
+plotResiduals(lme1,'fitted')
+
+find(residuals(lme1) < -200);
+find(residuals(lme1) >  200);
+
+%%
+lme1=  fitlme(tbl,'ForceRateFP~JerkSmartphone+Speed+BW+(1|Participant)+(JerkSmartphone-1|Participant)','Exclude',[61,66,97,102,110]);  % Mixed model with random effect intercept and coefficient 
+lme2=  fitlme(tbl,'ForceRateFP~JerkSmartphone^2+Speed+BW+(1|Participant)','Exclude',[61,66,97]);   % Mixed model with random effect intercept 
