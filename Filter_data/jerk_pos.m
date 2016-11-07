a07a68a5c3a9fae006f90e272ab8ff04ae4ccdb3
Nazirizadeh S <s.nazirizadeh@soton.ac.uk>
2016-11-07 23:51:56 +0000

    developed jerk function with just positive values

--- Filter_data/jerk_pos.m
+++ Filter_data/jerk_pos.m
@@ -1,6 +1,8 @@
-function [j_xyz] = jerk_xyz( t,v )
+function [j_pos, positives] = jerk_pos( t,v )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+% t= timestemp
+% v= acceleration vector of three dimentions 
 
 a=v(:,2:4);
 tnew=t/10^3;
@@ -8,7 +10,10 @@ sps=round(1/mean(tnew(2:end,1)-tnew(1:end-1,1)));
 n=length(tnew);
 j=zeros(n,3);
 j(2:end,:)=((a(2:end,:)-a(1:end-1,:))./(1/sps));
-j_xyz=sqrt(sum(j.^2,2));
-end
 
+positives = j(2:end,3) >= 0;
+
+j_pos=sqrt(sum(j.^2,2)) .* positives;
+
+end
 
