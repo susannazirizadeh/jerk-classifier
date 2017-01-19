3ee4631c0c8051809d0f9755115cc473dacff05a
Nazirizadeh S <s.nazirizadeh@soton.ac.uk>
2017-01-19 12:00:03 +0000

    Regression model for speed, bootstrapped and cross-validated

--- Classification/Classfication_outdoor.m
+++ Classification/Classfication_outdoor.m
@@ -10,11 +10,13 @@
 % 7. Plot the classification boundaries
 % 8. Evaluate Accuracy on New Data
  load results_loadrate
-button=1;
-C=([results_loadrate.outdoor{button}{1}(:,:);results_loadrate.outdoor{button}{2}(:,:);results_loadrate.outdoor{button}{3}(:,:);results_loadrate.outdoor{button}{4}(:,:);results_loadrate.outdoor{button}{5}(:,:);results_loadrate.outdoor{button}{6}(:,:);results_loadrate.outdoor{button}{7}(:,:);results_loadrate.outdoor{button}{8}(:,:);results_loadrate.outdoor{button}{9}(:,:);results_loadrate.outdoor{button}{10}(:,:);results_loadrate.outdoor{button}{11}(:,:);results_loadrate.outdoor{button}{12}(:,:)]);
+
+C(:,1:6)=([results_loadrate.outdoor{1}{1}(:,:);results_loadrate.outdoor{1}{2}(:,:);results_loadrate.outdoor{1}{3}(:,:);results_loadrate.outdoor{1}{4}(:,:);results_loadrate.outdoor{1}{5}(:,:);results_loadrate.outdoor{1}{6}(:,:);results_loadrate.outdoor{1}{7}(:,:);results_loadrate.outdoor{1}{8}(:,:);results_loadrate.outdoor{1}{9}(:,:);results_loadrate.outdoor{1}{10}(:,:);results_loadrate.outdoor{1}{11}(:,:);results_loadrate.outdoor{1}{12}(:,:)]);
+C(:,7)= ([results_loadrate.outdoor{2}{1}(:,1);results_loadrate.outdoor{2}{2}(:,1);results_loadrate.outdoor{2}{3}(:,1);results_loadrate.outdoor{2}{4}(:,1);results_loadrate.outdoor{2}{5}(:,1);results_loadrate.outdoor{2}{6}(:,1);results_loadrate.outdoor{2}{7}(:,1);results_loadrate.outdoor{2}{8}(:,1);results_loadrate.outdoor{2}{9}(:,1);results_loadrate.outdoor{2}{10}(:,1);results_loadrate.outdoor{2}{11}(:,1);results_loadrate.outdoor{2}{12}(:,1)]);
+Cnew=C(find(C(:,6)),:);
 %% Train SVM Classifier Using Custom Kernelfigure
-X=[C(:,6)];%,C(:,2)];
-Y=C(:,1); 
+X=[Cnew(:,6)];%,Cnew(:,2)];
+Y=Cnew(:,1); 
 MdlGau =fitrsvm(X,Y,'Standardize',true,'KernelFunction','gaussian');
 yfit=predict(MdlGau,X)
 
