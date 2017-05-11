c5cc754717cd38c44b6a44bb0f248eb197f2ad46
Nazirizadeh S <s.nazirizadeh@soton.ac.uk>
2017-05-11 16:40:28 +0100

    Used different lower and upper bound thresholds for the decision three

--- Tidy Classification/Categorizer.m
+++ Tidy Classification/Categorizer.m
@@ -6,20 +6,30 @@
 % 4. Test set, exluded participant actvity should be predicted with amount load rate estimate 
 %% 1. Define low, medium and high load rate
 load featurematrix_loadrate
+figure
 hist(featurematrix_loadrate(:,1),1000);
-low= featurematrix_loadrate(find(featurematrix_loadrate(:,1)<1.5e+7),1);
-med= featurematrix_loadrate(find(featurematrix_loadrate(:,1)>1.5e+7 & featurematrix_loadrate(:,1)<2.5e+7),1);
+title('Histogram of whole data set')
+xlabel('Load rate value')
+ylabel('Density')
+% low= featurematrix_loadrate(find(featurematrix_loadrate(:,1)<1.5e+7),1);
+% med= featurematrix_loadrate(find(featurematrix_loadrate(:,1)>1.5e+7 & featurematrix_loadrate(:,1)<2.5e+7),1);
+% high= featurematrix_loadrate(find(featurematrix_loadrate(:,1)>2.5e+7),1);
+low= featurematrix_loadrate(find(featurematrix_loadrate(:,1)<5.8253e+3),1);
+med= featurematrix_loadrate(find(featurematrix_loadrate(:,1)>5.8253e+3 & featurematrix_loadrate(:,1)<9.08e+3),1);
 high= featurematrix_loadrate(find(featurematrix_loadrate(:,1)>2.5e+7),1);
+ lower=5.8253e+6;
+ upper=9.08e+6;
 length(low);
 length(med);
 length(high);
+
 %% 2. Creat pie-charts for every single activty
 figure;
  featurematrix_loadrate=featurematrix_loadrate(find(featurematrix_loadrate(:,1)>0.1e+7),:);
 walk=featurematrix_loadrate(find(featurematrix_loadrate(:,11)==1),1);
-low_walk=walk(find(walk<1.5e+7),1);
-med_walk=walk(find(walk>1.5e+7 & walk<2.56e+7),1);
-high_walk=walk(find(walk(:,1)>2.5e+7),1);
+low_walk=walk(find(walk<lower),1);
+med_walk=walk(find(walk>lower & walk<upper),1);
+high_walk=walk(find(walk(:,1)>upper),1);
 X=[length(low_walk) length(med_walk) length(high_walk)];
 labels = {'Low','Medium','High'};
 ax1 = subplot(2,3,1);
@@ -28,8 +38,8 @@ title(ax1,'Walk');
 
 jog=featurematrix_loadrate(find(featurematrix_loadrate(:,11)==2),1);
 low_jog=jog(find(jog<1.5e+7),1);
-med_jog=jog(find(jog>1.5e+7 & jog<2.56e+7),1);
-high_jog=jog(find(jog(:,1)>2.5e+7),1);
+med_jog=jog(find(jog>lower & jog<upper),1);
+high_jog=jog(find(jog(:,1)>upper),1);
 Y=[length(low_jog) length(med_jog) length(high_jog)];
 labels = {'Low','Medium','High'};
 ax2 = subplot(2,3,2);
@@ -37,9 +47,9 @@ pie(ax2,Y,labels)
 title(ax2,'Jog');
 
 run=featurematrix_loadrate(find(featurematrix_loadrate(:,11)==3),1);
-low_run=run(find(run<1.5e+7),1);
-med_run=run(find(run>1.5e+7 & run<2.56e+7),1);
-high_run=run(find(run(:,1)>2.5e+7),1);
+low_run=run(find(run<lower),1);
+med_run=run(find(run>lower & run<upper),1);
+high_run=run(find(run(:,1)>upper),1);
 Z=[length(low_run) length(med_run) length(high_run)];
 labels = {'Low','Medium','High'};
 ax3 = subplot(2,3,3);
@@ -47,9 +57,9 @@ pie(ax3,Z,labels)
 title(ax3,'Run');
 
 slow_stairs=featurematrix_loadrate(find(featurematrix_loadrate(:,11)==4),1);
-low_slow_stairs=slow_stairs(find(slow_stairs<1.5e+7),1);
-med_slow_stairs=slow_stairs(find(slow_stairs>1.5e+7 & slow_stairs<2.56e+7),1);
-high_slow_stairs=slow_stairs(find(slow_stairs(:,1)>2.5e+7),1);
+low_slow_stairs=slow_stairs(find(slow_stairs<lower),1);
+med_slow_stairs=slow_stairs(find(slow_stairs>lower & slow_stairs<upper),1);
+high_slow_stairs=slow_stairs(find(slow_stairs(:,1)>upper),1);
 A=[length(low_slow_stairs) length(med_slow_stairs) length(high_slow_stairs)];
 labels = {'Low','Medium','High'};
 ax4 = subplot(2,3,4);
@@ -57,9 +67,9 @@ pie(ax4,A,labels)
 title(ax4,'Slow stairs');
 
 fast_stairs=featurematrix_loadrate(find(featurematrix_loadrate(:,11)==5),1);
-low_fast_stairs=fast_stairs(find(fast_stairs<1.5e+7),1);
-med_fast_stairs=fast_stairs(find(fast_stairs>1.5e+7 & fast_stairs<2.56e+7),1);
-high_fast_stairs=fast_stairs(find(fast_stairs(:,1)>2.5e+7),1);
+low_fast_stairs=fast_stairs(find(fast_stairs<lower),1);
+med_fast_stairs=fast_stairs(find(fast_stairs>lower & fast_stairs<upper),1);
+high_fast_stairs=fast_stairs(find(fast_stairs(:,1)>upper),1);
 B=[length(low_fast_stairs) length(med_fast_stairs) length(high_fast_stairs)];
 labels = {'Low','Medium','High'};
 ax5 = subplot(2,3,5);
@@ -130,33 +140,33 @@ for participant=[1:4 6:12]
     % Categorize in within true actvity
     figure;
     walk_part=Xtest(find(Xtest(:,21)==1),1);
-    low_walk_part=walk_part(find(walk_part<1.5e+7),1);
-    med_walk_part=walk_part(find(walk_part>1.5e+7 & walk_part<2.56e+7),1);
-    high_walk_part=walk_part(find(walk_part(:,1)>2.5e+7),1);
+    low_walk_part=walk_part(find(walk_part<lower),1);
+    med_walk_part=walk_part(find(walk_part>lower & walk_part<upper),1);
+    high_walk_part=walk_part(find(walk_part(:,1)>upper),1);
     X=[length(low_walk_part) length(med_walk_part) length(high_walk_part)];
     
     jog_part=Xtest(find(Xtest(:,21)==2),1);
-    low_jog_part=jog_part(find(jog_part<1.5e+7),1);
-    med_jog_part=jog_part(find(jog_part>1.5e+7 & jog_part<2.56e+7),1);
-    high_jog_part=jog_part(find(jog_part(:,1)>2.5e+7),1);
+    low_jog_part=jog_part(find(jog_part<lower),1);
+    med_jog_part=jog_part(find(jog_part>lower & jog_part<upper),1);
+    high_jog_part=jog_part(find(jog_part(:,1)>upper),1);
     Y=[length(low_jog_part) length(med_jog_part) length(high_jog_part)];
     
     run_part=Xtest(find(Xtest(:,21)==3),1);
-    low_run_part=run_part(find(run_part<1.5e+7),1);
-    med_run_part=run_part(find(run_part>1.5e+7 & run_part<2.56e+7),1);
-    high_run_part=run_part(find(run_part(:,1)>2.5e+7),1);
+    low_run_part=run_part(find(run_part<lower),1);
+    med_run_part=run_part(find(run_part>lower & run_part<upper),1);
+    high_run_part=run_part(find(run_part(:,1)>upper),1);
     Z=[length(low_run_part) length(med_run_part) length(high_run_part)];
     
     slow_stairs_part=Xtest(find(Xtest(:,21)==4),1);
-    low_slow_stairs_part=slow_stairs_part(find(slow_stairs_part<1.5e+7),1);
-    med_slow_stairs_part=slow_stairs_part(find(slow_stairs_part>1.5e+7 & slow_stairs_part<2.56e+7),1);
-    high_slow_stairs_part=slow_stairs_part(find(slow_stairs_part(:,1)>2.5e+7),1);
+    low_slow_stairs_part=slow_stairs_part(find(slow_stairs_part<lower),1);
+    med_slow_stairs_part=slow_stairs_part(find(slow_stairs_part>lower & slow_stairs_part<upper),1);
+    high_slow_stairs_part=slow_stairs_part(find(slow_stairs_part(:,1)>upper),1);
     A=[length(low_slow_stairs_part) length(med_slow_stairs_part) length(high_slow_stairs_part)];
     
     fast_stairs_part=Xtest(find(Xtest(:,21)==5),1);
-    low_fast_stairs_part=fast_stairs_part(find(fast_stairs_part<1.5e+7),1);
-    med_fast_stairs_part=fast_stairs_part(find(fast_stairs_part>1.5e+7 & fast_stairs_part<2.56e+7),1);
-    high_fast_stairs_part=fast_stairs_part(find(fast_stairs_part(:,1)>2.5e+7),1);
+    low_fast_stairs_part=fast_stairs_part(find(fast_stairs_part<lower),1);
+    med_fast_stairs_part=fast_stairs_part(find(fast_stairs_part>lower & fast_stairs_part<2.56e+7),1);
+    high_fast_stairs_part=fast_stairs_part(find(fast_stairs_part(:,1)>upper),1);
     B=[length(low_fast_stairs_part) length(med_fast_stairs_part) length(high_fast_stairs_part)];
     
     y=[X;Y;Z];%;A;B;];
@@ -164,39 +174,40 @@ for participant=[1:4 6:12]
     bar(ax1,y,'stacked');
     name = {'Walking';'Jogging';'Running'};%;'Stairs slow';'Stairs fast'};
     set(gca,'xticklabel',name);
+    ylabel('Density')
     ylim([0 300])
     legend('Low load rate','Medium load rate','High load rate');
     title(['True activty, Participant ',num2str(participant)]);
     
     % Categorize in within predicted activity
     walk_part=Xtest(find(Xtest(:,25)==1),1);
-    low_walk_part=walk_part(find(walk_part<1.5e+7),1);
-    med_walk_part=walk_part(find(walk_part>1.5e+7 & walk_part<2.56e+7),1);
-    high_walk_part=walk_part(find(walk_part(:,1)>2.5e+7),1);
+    low_walk_part=walk_part(find(walk_part<lower),1);
+    med_walk_part=walk_part(find(walk_part>lower & walk_part<upper),1);
+    high_walk_part=walk_part(find(walk_part(:,1)>upper),1);
     X=[length(low_walk_part) length(med_walk_part) length(high_walk_part)];
     
     jog_part=Xtest(find(Xtest(:,25)==2),1);
-    low_jog_part=jog_part(find(jog_part<1.5e+7),1);
-    med_jog_part=jog_part(find(jog_part>1.5e+7 & jog_part<2.56e+7),1);
-    high_jog_part=jog_part(find(jog_part(:,1)>2.5e+7),1);
+    low_jog_part=jog_part(find(jog_part<lower),1);
+    med_jog_part=jog_part(find(jog_part>lower & jog_part<upper),1);
+    high_jog_part=jog_part(find(jog_part(:,1)>upper),1);
     Y=[length(low_jog_part) length(med_jog_part) length(high_jog_part)];
     
     run_part=Xtest(find(Xtest(:,25)==3),1);
-    low_run_part=run_part(find(run_part<1.5e+7),1);
-    med_run_part=run_part(find(run_part>1.5e+7 & run_part<2.56e+7),1);
-    high_run_part=run_part(find(run_part(:,1)>2.5e+7),1);
+    low_run_part=run_part(find(run_part<lower),1);
+    med_run_part=run_part(find(run_part>lower & run_part<upper),1);
+    high_run_part=run_part(find(run_part(:,1)>upper),1);
     Z=[length(low_run_part) length(med_run_part) length(high_run_part)];
     
     slow_stairs_part=Xtest(find(Xtest(:,25)==4),1);
-    low_slow_stairs_part=slow_stairs_part(find(slow_stairs_part<1.5e+7),1);
-    med_slow_stairs_part=slow_stairs_part(find(slow_stairs_part>1.5e+7 & slow_stairs_part<2.56e+7),1);
-    high_slow_stairs_part=slow_stairs_part(find(slow_stairs_part(:,1)>2.5e+7),1);
+    low_slow_stairs_part=slow_stairs_part(find(slow_stairs_part<lower),1);
+    med_slow_stairs_part=slow_stairs_part(find(slow_stairs_part>lower & slow_stairs_part<upper),1);
+    high_slow_stairs_part=slow_stairs_part(find(slow_stairs_part(:,1)>upper),1);
     A=[length(low_slow_stairs_part) length(med_slow_stairs_part) length(high_slow_stairs_part)];
     
     fast_stairs_part=Xtest(find(Xtest(:,25)==5),1);
-    low_fast_stairs_part=fast_stairs_part(find(fast_stairs_part<1.5e+7),1);
-    med_fast_stairs_part=fast_stairs_part(find(fast_stairs_part>1.5e+7 & fast_stairs_part<2.56e+7),1);
-    high_fast_stairs_part=fast_stairs_part(find(fast_stairs_part(:,1)>2.5e+7),1);
+    low_fast_stairs_part=fast_stairs_part(find(fast_stairs_part<lower),1);
+    med_fast_stairs_part=fast_stairs_part(find(fast_stairs_part>lower & fast_stairs_part<upper),1);
+    high_fast_stairs_part=fast_stairs_part(find(fast_stairs_part(:,1)>upper),1);
     B=[length(low_fast_stairs_part) length(med_fast_stairs_part) length(high_fast_stairs_part)];
     
     y=[X;Y;Z];%;A;B;];
@@ -204,6 +215,7 @@ for participant=[1:4 6:12]
     bar(ax2,y,'stacked');
     name = {'Walking';'Jogging';'Running'};%;'Stairs slow';'Stairs fast'};
     set(gca,'xticklabel',name);
+    ylabel('Denisty')
     ylim([0 300])
     legend('Low load rate','Medium load rate','High load rate');
     title(['Predicted running speed, Participant ',num2str(participant)]);
@@ -293,6 +305,7 @@ for participant=[1:4 6:12]
     bar(ax1,y,'stacked');
     name = {'inclineup';'inclinedown';'stairsup';'stairsdown'};
     set(gca,'xticklabel',name);
+    ylabel('Denisty')
     ylim([0 50])
     legend('Low load rate','Medium load rate','High load rate');
     title(['True incline and stairs, Participant ',num2str(participant)]);
@@ -345,6 +358,7 @@ for participant=[1:4 6:12]
     bar(ax2,y,'stacked');
     name = {'inlcineup';'inclinedown';'stairsup';'stairsdown'};
     set(gca,'xticklabel',name);
+    ylabel('Denisty')
     ylim([0 50])
     legend('Low load rate','Medium load rate','High load rate');
     title(['Prediced incline and stairs, Participant ',num2str(participant)]); 
@@ -432,8 +446,9 @@ for participant=[1:4 6:12]
     y=[X;Y;Z];%;A;B;C;D];
     ax1 = subplot(2,1,1);
     bar(ax1,y,'stacked');
-    name = {'asphal';'grass';'soil';'inlcineup';'inclinedown';'stairsup';'stairsdown'};
+    name = {'asphal';'grass';'gravel path';'inlcineup';'inclinedown';'stairsup';'stairsdown'};
     set(gca,'xticklabel',name);
+    ylabel('Denisty')
 %     ylim([0 320])
     legend('Low load rate','Medium load rate','High load rate');
     title(['True terrain, Participant ',num2str(participant)]);
@@ -484,8 +499,9 @@ for participant=[1:4 6:12]
     y=[X;Y;Z];%;A;B;C;D];
     ax2 = subplot(2,1,2);
     bar(ax2,y,'stacked');
-    name = {'asphal';'grass';'soil';'inlcineup';'inclinedown';'stairsup';'stairsdown'};
+    name = {'asphal';'grass';'gravel path';'inlcineup';'inclinedown';'stairsup';'stairsdown'};
     set(gca,'xticklabel',name);
+    ylabel('Denisty')
 %     ylim([0 320])
     legend('Low load rate','Medium load rate','High load rate');
     title(['Prediced terrain, Participant ',num2str(participant)]); 
